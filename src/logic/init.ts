e871680215bf20293e1732ad60eea89b102d7641
Ágata Ordano <ordanoagata@gmail.com>
2022-04-07 13:37:02 -0300

    fix(i18n): fix base intl components and refactor them to actually work as intended

--- src/logic/init.ts
+++ src/logic/init.ts
@@ -1,4 +1,4 @@
-import * as Mag from "magnetic-js";
+import * as ZT from "ziptied";
 import i18n_en from "../i18n/en.json";
 import i18n_es from "../i18n/es.json";
 
@@ -14,53 +14,54 @@ const langs: Langs = {
 export const state = {
     UI: {
         media: {
-            isMobile: new Mag.State(false),
+            isMobile: new ZT.State(false),
         },
         queries: {
-            load: new Mag.Events.ActiveEvent<boolean>("load", (event) => true),
-            // selectLang: new MagCustomEvent<string, typeof window>(
-            //     "language",
-            //     window,
-            //     (value, data: CustomEvent) => {
-            //         return data.detail;
-            //     }
-            // ),
-            mobile: new Mag.Events.WidthQuery(768),
+            load: new ZT.Events.ActiveEvent<boolean>("load", (event) => true),
+            mobile: new ZT.Events.WidthQuery(768),
         },
+        lang: new ZT.State("en"),
+        availableLangs: langs,
     },
     data: {},
     nodes: {
-        potato: new Mag.Node("potato"),
+        potato: new ZT.Node("potato"),
     },
     components: {
-        i18n: new Mag.Component("i18n"),
-        langSelector: new Mag.Component("langSelector"),
+        i18n: new ZT.Component("i18n", (node, payload) => {
+            node.dataset.ztI18nKey = node.innerText;
+            return node;
+        }),
+        langSelector: new ZT.Component("langSelector"),
     },
 };
+
+interface StatefulWindow extends Window {
+    state: typeof state;
+}
 
 export const init = (): void => {
     state.UI.queries.mobile.subscribe("menu", {
         next: (matches) => state.UI.media.isMobile.update(matches),
     });
 
-    const changeLangEvent = (lang: string) =>
-        new CustomEvent("i18n_update", { detail: lang });
-
     state.components.langSelector.onEventLocal("click", (node) => {
-        const lang = node.getAttribute("data-mag-i18n-lang");
+        const lang = node.getAttribute("data-zt-i18n-lang");
         if (lang) {
-            window.dispatchEvent(changeLangEvent(lang));
+            console.info(`updated lang to ${lang}`);
+            (window as unknown as StatefulWindow).state.UI.lang.update(lang);
         }
         return node;
     });
-
-    state.components.i18n.onEventGlobal(
+    state.components.i18n.addSideEffect(
         "i18n_update",
-        (node: HTMLElement, payload: CustomEvent<keyof typeof langs>) => {
-            const i18n_key = node.id.slice(2);
-            const lang = payload.detail;
-            node.innerHTML = langs[lang][i18n_key];
+        (node: HTMLElement, lang: keyof typeof langs) => {
+            console.info("fired i18n_update effect");
+            node.innerText = (
+                window as unknown as StatefulWindow
+            ).state.UI.availableLangs[lang][node.dataset.ztI18nKey];
             return node;
-        }
+        },
+        state.UI.lang
     );
 };
