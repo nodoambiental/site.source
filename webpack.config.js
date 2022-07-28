568c9112308d3b11a4b89fd4538174858aa6878a
Ágata Ordano <aordano@protonmail.com>
2022-07-27 21:54:46 -0300

    tooling(webpack): include zola in webpack chain and add serve mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,98 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const P = require("./util/paths.json");
 const tooling = require("./util/tooling.js");
+const childProcess = require("child_process");
 
 const isProduction = process.env.NODE_ENV == "production";
+const isWatchMode = process.env.WATCH;
+
+const paths = {
+    pug: "./src/templates/",
+    out: "build/zola",
+    static: {
+        dir: "src/",
+        sources: ["static/", "data/", "config.toml", "theme.toml"],
+        out: "",
+    },
+    entry: {
+        dir: "./src/",
+        file: "bundle",
+    },
+    bundle: {
+        path: "build/zola/bundle.js",
+        out: "build/zola/static/bundle.js",
+    },
+};
 
 const config = {
     target: "web",
-    entry: `${P.entry.dir}${P.entry.file}.ts`,
+    entry: `${paths.entry.dir}${paths.entry.file}.ts`,
     output: {
-        path: path.resolve(__dirname, P.out),
+        path: path.resolve(__dirname, paths.out),
         clean: true,
-        filename: `${P.entry.file}.js`,
+        filename: `${paths.entry.file}.js`,
     },
     plugins: [
-        tooling.copyBundle(P.bundle.path, P.bundle.out),
-        tooling.statics(P.static.sources, P.static.dir, P.static.out),
-        ...tooling.pug(P.pug),
+        tooling.copyBundle(paths.bundle.path, paths.bundle.out),
+        tooling.statics(
+            paths.static.sources,
+            paths.static.dir,
+            paths.static.out
+        ),
+        ...tooling.pug(paths.pug),
+        {
+            apply: (compiler) => {
+                compiler.hooks.afterEmit.tap("ZolaCompile", (compilation) => {
+                    const zola = childProcess.spawn("zola", [
+                        "--root",
+                        "build/zola",
+                        "build",
+                    ]);
+
+                    zola.stdout.on("data", function (data) {
+                        process.stdout.write(data);
+                    });
+                    zola.stderr.on("data", function (data) {
+                        process.stderr.write(data);
+                    });
+                });
+
+                compiler.hooks.done.tap("DevServer", (compilation) => {
+                    if (isWatchMode) {
+                        const reload = childProcess.spawn("reload", [
+                            "--browser",
+                            "--dir",
+                            "./build/dist/",
+                            "--port",
+                            "9000",
+                            "--verbose",
+                        ]);
+
+                        reload.stdout.on("data", function (data) {
+                            process.stdout.write(data);
+                        });
+                        reload.stderr.on("data", function (data) {
+                            process.stderr.write(data);
+                        });
+
+                        childProcess.exec(
+                            `cp "${path.resolve(
+                                __dirname,
+                                "build",
+                                "zola",
+                                "bundle.js"
+                            )}" "${path.resolve(
+                                __dirname,
+                                "build",
+                                "dist",
+                                "bundle.js"
+                            )}"`
+                        );
+                    }
+                });
+            },
+        },
     ],
     module: {
         rules: [
